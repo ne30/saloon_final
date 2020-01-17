cc96017f9434899e49f61a5cb19bd9942698d0a4
ne30 <neeraj.ace.30@gmail.com>
2020-01-17 20:16:08 +0530

    First commit adding all files

---
+++ salonspaw.com/wp-content/themes/salon-spa-w/js/teamnav.js
@@ -0,0 +1,161 @@
+	function switchVisible() {
+            if (document.getElementById('team-group-1')) {
+
+                if (document.getElementById('team-group-1').style.display == 'none') {
+                    document.getElementById('team-group-1').style.display = 'flex';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+				    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+               
+                else {
+                    document.getElementById('team-group-1').style.display = 'flex';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    				    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+
+                }
+            }
+	}
+	function switchVisible2() {
+            if (document.getElementById('team-group-2')) {
+
+                if (document.getElementById('team-group-2').style.display == 'none') {
+                    document.getElementById('team-group-2').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+
+                }
+                else {
+                    document.getElementById('team-group-2').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+            }
+	}
+	function switchVisible3() {
+            if (document.getElementById('team-group-3')) {
+
+                if (document.getElementById('team-group-3').style.display == 'none') {
+                    document.getElementById('team-group-3').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+               
+                else {
+                    document.getElementById('team-group-3').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+            }
+	}
+	function switchVisible4() {
+            if (document.getElementById('team-group-4')) {
+
+                if (document.getElementById('team-group-4').style.display == 'none') {
+                    document.getElementById('team-group-4').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+               
+                else {
+                    document.getElementById('team-group-4').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-5').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+            }
+	}
+		function switchVisible5() {
+            if (document.getElementById('team-group-4')) {
+
+                if (document.getElementById('team-group-5').style.display == 'none') {
+                    document.getElementById('team-group-5').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+               
+                else {
+                    document.getElementById('team-group-5').style.display = 'flex';
+                    document.getElementById('team-group-1').style.display = 'none';
+                    document.getElementById('team-group-2').style.display = 'none';
+                    document.getElementById('team-group-3').style.display = 'none';
+                    document.getElementById('team-group-4').style.display = 'none';
+                    var x = document.getElementById("dropdown-content");
+				    if (x.style.display === "none") {
+				        x.style.display = "block";
+				    } else {
+				        x.style.display = "none";
+				    }
+                }
+            }
+	}
